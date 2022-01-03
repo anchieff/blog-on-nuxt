b13a211b0567f978f8f770e6c150baeb215a3404
anchieff <info@annanekrasova.ru>
2022-01-03 19:04:25 +0400

    init: start project

---
+++ components/posts/PostList.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="featured-posts">
+    <PostPreview
+      id="1"
+      :is-admin="isAdmin"
+      thumbnail="https://cdn.thewirecutter.com/wp-content/media/2021/03/adopting-a-cat-2048px-06052.jpg?auto=webp&quality=60&crop=1.91:1&width=1200"
+      title="Hello World!"
+      previewText="This is my first post!"
+    />
+    <PostPreview
+      id="1"
+      :is-admin="isAdmin"
+      thumbnail="https://cdn.thewirecutter.com/wp-content/media/2021/03/adopting-a-cat-2048px-06052.jpg?auto=webp&quality=60&crop=1.91:1&width=1200"
+      title="Hello World!"
+      previewText="This is my first post!"
+    />
+    <PostPreview
+      id="1"
+      :is-admin="isAdmin"
+      thumbnail="https://cdn.thewirecutter.com/wp-content/media/2021/03/adopting-a-cat-2048px-06052.jpg?auto=webp&quality=60&crop=1.91:1&width=1200"
+      title="Hello World!"
+      previewText="This is my first post!"
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from "@/components/posts/PostPreview";
+
+export default {
+  components: {
+    PostPreview,
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from "@/components/admin/AdminPostForm";
+
+export default {
+  layout: "admin",
+  components: {
+    AdminPostForm,
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: "Anna",
+        title: "My awesome Post",
+        content: "Super amazing, thanks for that!",
+        thumbnail:
+          "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/newscms/2021_26/3487828/210630-stock-cat-bed-ew-245p.jpg",
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
